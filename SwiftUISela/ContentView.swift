88f802ff45516e34c5ac81659307ef00017b7417
jiaoguohui <jiaoguohui@sensetime.com>
2021-12-06 14:57:22 +0800

    Initial Commit

---
+++ SwiftUISela/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUISela
+//
+//  Created by 焦国辉 on 2021/12/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUISela/SwiftUISelaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISelaApp.swift
+//  SwiftUISela
+//
+//  Created by 焦国辉 on 2021/12/6.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISelaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
